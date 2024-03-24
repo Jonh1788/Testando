97636e4f80bfa96ba5b589f912e42d72706ce5d0
Jonathan <jonathan100flamengo@gmail.com>
2024-03-24 15:11:17 -0300

    Added more control over exclusions

--- app/Services/LimparBanco.php
+++ app/Services/LimparBanco.php
@@ -10,7 +10,20 @@ class LimparBanco
     {
         \DB::statement('SET FOREIGN_KEY_CHECKS=0');
         
-        Appconfig::where('depositou', '>', 60)->where('data_cadastro', '<', now()->subDays(2))->delete();
+        Appconfig::where('depositou', '>', 60)->each(function($appconfig) {
+
+            $ultimoDeposito = \DB::table("confirmar_deposito")
+            ->where('email', $appconfig->email)
+            ->where("status", "PAID_OUT")
+            ->orderBy("data", "desc")
+            ->first();
+
+            if($ultimoDeposito->data < now()->subDays(2)){
+                $appconfig->delete();
+            }
+        });
+
+        
         Appconfig::where('depositou', 0)->where('data_cadastro', '<', now()->subDays(3))->delete();
 
         \DB::statement('SET FOREIGN_KEY_CHECKS=1');
